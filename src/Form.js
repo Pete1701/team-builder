9e60d3bad76f7a638cab5d8b295043380171fd5c
Pete1701 <pete.pertongonas@pirron.one>
2020-02-26 22:55:16 +0100

    add form

---
+++ src/Form.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './Form.css';
+
+const Form = props => {
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        role: ''
+    });
+
+    const handleChanges = event => {
+        console.log(data);
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const submitForm = event => {
+        event.preventDefault();
+        props.addNewNote(data);
+        setData({ name: "", email: "", role: "" });
+      };
+
+    return (
+        <form className='formName' onSubmit={submitForm}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Your name"
+            name="name"
+            onChange={handleChanges}
+            value={data.name}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            id="note"
+            type="email"
+            placeholder="Your email"
+            name="email"
+            onChange={handleChanges}
+            value={data.email}
+          />
+          <label htmlFor="role">Role</label>
+          <select>
+            id="role"
+            name="role"
+            <option value='1'>Front End</option>
+            <option value='2'>Back End</option>
+            <option value='3'>Designer</option> 
+            onChange={handleChanges}
+            value={data.role}
+          </select>
+          {/* <button type="submit">Add Note</button> */}
+        </form>
+    );
+};
+
+export default Form;
